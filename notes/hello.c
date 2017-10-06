70f7a6709928192ffc3dbb64e1a9a0574fae68dc
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-10-06 12:29:59 -0600

    first c programs

---
+++ notes/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("hello world!\n");
+}
+
+//clang in terminal to compile
+//then use ./a.out (a.out will be the default name if wr dont specify it)
+//clang -o programName file (we give it the name we wnat out program to be then the file)
+// cli make fileName (same as above its a convinience program that will compile and name program for us)
---
+++ notes/string.c
@@ -0,0 +1,11 @@
+//#include <cs50.h> this file wont run because c doesn't know what strings are
+#include <stdio.h>
+
+
+int main(void)
+{
+  string name = get_string();
+  printf("hello, %s\n", name);
+}
+
+//
