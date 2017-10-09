98944daafa4dc9fd8c5073c26eaa0d26b0f570f0
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-10-09 14:38:18 -0600

    added notes about permutation and problems taht arize with data representation

--- notes/hello.c
+++ notes/hello.c
@@ -8,3 +8,7 @@ int main(void)
 //then use ./a.out (a.out will be the default name if wr dont specify it)
 //clang -o programName file (we give it the name we wnat out program to be then the file)
 // cli make fileName (same as above its a convinience program that will compile and name program for us)
+//one wierd downside of data representation is that floats are still finite
+//caused arian 5 rocket explosion and patriot missile exploded killing us soldiers
+//permutation is the representation of 0 and 1's.
+//overflow and inprecision cna cause terrible errors in programs
