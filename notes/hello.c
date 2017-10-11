2215de5cbed2d743f9cbc739adca6de718746b5a
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-10-11 15:02:35 -0600

    notes on preprocessing compileing assemnling and linking

--- notes/hello.c
+++ notes/hello.c
@@ -12,3 +12,32 @@ int main(void)
 //caused arian 5 rocket explosion and patriot missile exploded killing us soldiers
 //permutation is the representation of 0 and 1's.
 //overflow and inprecision cna cause terrible errors in programs
+
+
+/*
+impercision = 1/10 will give you .1000000055566 (ints not percice);
+overflow if a number gets too large it can overflow into a negitive number.
+
+
+\n for new line
+\r for carage return
+\" for quotes inside strings
+\' same as above
+
+= asignment operator
+== equality operator
+
+in C to compair a sing charictor or char we have to use single quotes. otherwise for strings we can use doublequotes
+
+
+preprocesing: does the find ond replace
+compileing: takes your sourse code ( C JS Python) and turns it into assembly code
+asembling: turns your code into zeros and ones
+linking: take all 0 and 1's and any other libraries we brought in adn give us one file of 0 and 1's.
+
+half the struggle is getting better at seeing patterns and erros and working through thoughs patterns
+
+
+
+
+*/
